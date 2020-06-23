b1fb6968fdea8b56cf04e417fe98fcffffca54da
lisandroprada <lisandro.prada@gmail.com>
2020-06-23 17:57:17 -0300

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 
@@ -9,38 +11,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(require('./routes/usuario'));
 
 const port = process.env.PORT;
 
-app.get('/usuario', (req, res) => {
-    res.json('get usuario');
-});
-
-app.post('/usuario', (req, res) => {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-});
-
-app.put('/usuario/:id', (req, res) => {
-
-    let id = req.params.id;
-    res.json({
-        id
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (err, res) => {
+        if (err) throw err;
+        console.log('Base de datos ONLINE');
     });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('Borrar')
-});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+// Mongo Atlas password 
+// User: lisandro
+// Password: vrvci2LdHQYNRWNq
+
+// mongodb+srv://lisandro:<password>@cluster0-7e9ez.gcp.mongodb.net/cafe
