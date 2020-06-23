363a8b82af45a733a98b501bc3bf452e96b07e71
lisandroprada <lisandro.prada@gmail.com>
2020-06-23 18:14:30 -0300

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,9 +16,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe'
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://lisandro:vrvci2LdHQYNRWNq@cluster0-7e9ez.gcp.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
