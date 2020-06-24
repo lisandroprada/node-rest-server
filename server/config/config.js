cdee12059f0b00ee8091cacf450b78eaa7f9ca00
lisandroprada <lisandro.prada@gmail.com>
2020-06-24 13:13:56 -0300

    Google Sign In

--- server/config/config.js
+++ server/config/config.js
@@ -1,30 +1,20 @@
-//
 // Puerto
-// 
 process.env.PORT = process.env.PORT || 3000;
 
-//
 // Entorno
-// 
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-//
 // Vencimiento del token
 // 60 segundos x 60 minutos
-
 process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 
-
-//
 // Seed
-//
-
 process.env.SEED = process.env.SEED || 'este-es-el-seed-de-desarrollo';
 
-//
+// Google Client ID
+process.env.CLIENT_ID = process.env.CLIENT_ID || '804033958350-2qtss2afvj7id6ktv6tgu3l48lmg2moh.apps.googleusercontent.com';
+
 // Base de datos
-// 
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
